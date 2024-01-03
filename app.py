b1f8f457ed26054e02801237fa9a99d5ebcdacbb
Maxim Barbe <barbe.maxim@gmail.com>
2024-01-03 15:10:46 -0500

    work on forms

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, request
 from forms import registerForm
 from flask_wtf import CSRFProtect
-from flask_login import LoginManager
+from flask_bcrypt import Bcrypt, generate_password_hash
+from flask_login import LoginManager, login_required
 from config import Config
 from models import User, Trade
+from database import db
 
 # Initialisation of a user
 user = None
@@ -15,7 +17,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 login_manager = LoginManager(app)
 csrf = CSRFProtect(app)
+db.init_app(app)
+# Setup Bcrypt for storing passwords safely
+bcrypt = Bcrypt(app)
 
+# Callback function for getting a user
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
@@ -32,11 +38,20 @@ def index():
 def register():
     register_form = registerForm(request.form)
     if request.method == "POST" and register_form.validate():
+        # All verifications needed to make sure the data is valid
         if (register_form.check_email_equal(register_form.email.data, register_form.confirm_email.data) and
             register_form.check_name_length(register_form.name.data) and register_form.check_email_length(register_form.email.data) and
             register_form.check_password_length(register_form.password.data) and register_form.check_valid_email(register_form.email.data) and
-            register_form.check_valid_password(register_form.password.data)):
-            return redirect("/")
+            register_form.check_valid_password(register_form.password.data)) and not register_form.check_user_already_exists(register_form.email.data):
+            # Adding a new user to the database
+            db.session.add(User(
+                name = register_form.name.data,
+                email = register_form.email.data,
+                password = generate_password_hash(register_form.password.data, 12),
+                authenticated = True
+            ))     
+            db.session.commit()
+            return redirect("/login")       
     return render_template("register.html", form=register_form)
     
 
@@ -48,7 +63,9 @@ def login():
     return "<b>Login Page</b>"
 
 # Logout route, will remove the user from the session and redirect to base directory
+
 @app.route("/logout")
+@login_required
 def logout():
     
     return redirect("/")
--- forms.py
+++ forms.py
@@ -4,6 +4,8 @@ import re
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import ValidationError
+from database import db
+from models import User
 
 # Regular expression to check email validity
 email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')  
@@ -12,10 +14,11 @@ email_regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z
 # Password must contain a minimum of 8 characters, at least one letter, one number and and one special character
 password_regex = re.compile(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$")
 
+# Form used for the register page
 class registerForm(FlaskForm):
     name = StringField(label="Name", description="Name")
-    email = StringField(label="Email", validators=None, description="Email")
-    confirm_email = StringField(label="confirm_email", validators=None, description="Confirm email")
+    email = StringField(label="Email", description="Email")
+    confirm_email = StringField(label="confirm_email", description="Confirm email")
     password = PasswordField(label="Password", description="Password")
     submit = SubmitField(label="Submit")
     
@@ -60,3 +63,22 @@ class registerForm(FlaskForm):
             self.password.errors += (ValidationError("Password must be less than 100 characters"),)
             return False
         return True
+    
+    def check_user_already_exists(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user != None:
+            self.email.errors += (ValidationError("Email already exists."),)
+        return True if user != None else False
+    
+# Form used for the login page
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", description="Email")
+    password = PasswordField(label="Password", description="Password")
+    submit = SubmitField(label="Submit")
+
+    # Email verification
+    def check_email_exists(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user == None:
+            self.email.errors += (ValidationError("Email does not exist."),)
+        return False if user == None else True
